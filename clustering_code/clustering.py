3dbc7d1fa43502488d8352dea927a0d8985b9c54
Zhen Zhai <zzhai@eng.ucsd.edu>
2016-08-03 15:15:26 -0700

    update clustering code

--- clustering_code/clustering.py
+++ clustering_code/clustering.py
@@ -12,6 +12,43 @@ from itertools import combinations
 from datetime import datetime as dt
 from datetime import timedelta as td
 
+def format_cluster(n, part):
+    time = dt.strptime(n[-1], "%Y-%m-%d %H:%M:%S")
+    # we create one signature 'redirect'
+    # that the hints should redirect students to previous parts
+    if part == '1' and user_id != n[3]:
+        user_id = n[3]
+        ini_time_str = n[-1]
+    if part != '1':
+        user_id = n[3]
+        ## Some students didn't complete part 1, need to check all parts before the current part
+        k = 1
+        while str(k) != part and not data_timestamp[user_id][(problem_id, str(k))]:
+            k += 1
+        if str(k) == part:
+            ini_time_str = n[-1]
+        else:
+            ini_time_str = data_timestamp[user_id][(problem_id, str(k))][1]
+    ini_time = dt.strptime(ini_time_str, "%Y-%m-%d %H:%M:%S")
+
+    dependency_timestamp = [dt.strptime(data_timestamp[user_id][(problem_id, d)][1], "%Y-%m-%d %H:%M:%S") for d in dependency_list if data_timestamp[user_id][(problem_id, d)]]
+    if any(time < t for t in dependency_timestamp):
+        problem_clusters[part]['redirect'].append((n[0],n[1]))
+    else:
+        if not user_id in no_match_cluster:
+            no_match_cluster[user_id] = {'first_attempt':ini_time_str}
+            for d in dependency_list+[part]:
+                if data_timestamp[user_id][(problem_id, d)]:
+                    correct_attempt = data_timestamp[user_id][(problem_id, d)][0]
+                    t = dt.strptime(data_timestamp[user_id][(problem_id, d)][1], "%Y-%m-%d %H:%M:%S")
+                    key = 'part' + str(d) + '_correct_attempt'
+                    no_match_cluster[user_id][key] = [str(t-ini_time), correct_attempt]
+            t = dt.strptime(n[-1], "%Y-%m-%d %H:%M:%S")
+            no_match_cluster[user_id]['part'+str(part)+'_incorrect_attempt'] = [(str(t-ini_time), n[1])]
+        else:
+            t = dt.strptime(n[-1], "%Y-%m-%d %H:%M:%S")
+            no_match_cluster[user_id]['part'+str(part)+'_incorrect_attempt'] += [(str(t-ini_time), n[1])]
+
 def cluster(Week, problem_id):
     ## We only care about attempts after 3 minutes in nothing cluster
     min_diff = 3
@@ -32,6 +69,8 @@ def cluster(Week, problem_id):
     problem_clusters = {}
     no_match_clusters = {}
     for part in params:
+        if len(params[part]) == 0:
+            continue
         problem_clusters[part]=defaultdict(list)
         no_match = []
         for p in params[part]:
@@ -47,14 +86,21 @@ def cluster(Week, problem_id):
                 if att * ans < 0:
                     problem_clusters[part]['Wrong_Sign'] += [(p['answer'], p['attempt'])]
                     continue
-                elif float(ans).is_integer() and not float(att).is_integer():
-                    problem_clusters[part]['Fraction'] += [(p['answer'], p['attempt'])]
-                    continue
+                else:
+                    convert_fail = False
+                    try:
+                        float(att)
+                    except OverflowError:
+                        print "Can't convert to float"
+                        convert_fail = True
+                    if not convert_fail and float(ans).is_integer() and not float(att).is_integer():
+                        problem_clusters[part]['Fraction'] += [(p['answer'], p['attempt'])]
+                        continue
             final_pairs = cluster_functions.find_matches(p)
             if len(final_pairs)>0:
                 sorted_final = sorted(final_pairs,key=lambda x: x[0])
                 sorted_node = [s[0] for s in sorted_final]
-                problem_clusters[part][str(sorted_node)] += [(p['answer'], p['attempt'], sorted_final)]
+                problem_clusters[part][str(sorted_node)] += [(p['answer'], p['attempt'], sorted_final, p['user_id'], p['user_var'], p['timestamp'])]
             else:
                 if p['attempt'].isdigit() or len(p['att_tree']) == 1:
                     problem_clusters[part]['Digits'] += [(p['answer'], p['attempt'])]
@@ -63,7 +109,7 @@ def cluster(Week, problem_id):
 
         ## Cluster no match
         no_match_cluster = {}
-        dependency_list_input = raw_input('What parts are prerequisite for part' + str(part) + ':')
+        dependency_list_input = raw_input('What parts are prerequisite for part' + str(part) + ':(seperate with space)')
         dependency_list = [i for i in dependency_list_input.split()]
         print 'part' + str(part) + ' depends on ', dependency_list
         user_id = ''
@@ -103,6 +149,7 @@ def cluster(Week, problem_id):
                 else:
                     t = dt.strptime(n[-1], "%Y-%m-%d %H:%M:%S")
                     no_match_cluster[user_id]['part'+str(part)+'_incorrect_attempt'] += [(str(t-ini_time), n[1])]
+
         ## Remove attempts within min_diff defined at the top
         updated_cluster = {}
         for user in no_match_cluster:
